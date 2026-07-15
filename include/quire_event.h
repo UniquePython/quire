1a1c6d4ac28bbdb93d8ef7538e83a45cd25b8207
Ananyo Bhattacharya <169988112+UniquePython@users.noreply.github.com>
2026-07-15 14:21:28 +0530

    Write event abstraction header

---
+++ include/quire_event.h
@@ -0,0 +1,52 @@
+#ifndef QUIRE_EVENT_H_
+#define QUIRE_EVENT_H_
+
+#include "quire_types.h"
+
+typedef enum
+{
+    QUIRE_EVENT_KEY,
+    QUIRE_EVENT_TEXT,
+    QUIRE_EVENT_MOUSE_BUTTON,
+    QUIRE_EVENT_MOUSE_MOVE,
+    QUIRE_EVENT_RESIZE,
+    QUIRE_EVENT_REDRAW,
+    QUIRE_EVENT_CLOSE,
+} QuireEventType;
+
+typedef struct
+{
+    QuireEventType type;
+    union
+    {
+        struct
+        {
+            u32 key;
+            bool pressed;
+            u32 modifiers;
+        } key;
+        struct
+        {
+            char text[8];
+            usize length;
+        } text;
+        struct
+        {
+            int x, y;
+            u32 button;
+            bool pressed;
+            u32 modifiers;
+        } mouseButton;
+        struct
+        {
+            int x, y;
+            u32 modifiers;
+        } mouseMove;
+        struct
+        {
+            u32 width, height;
+        } resize;
+    } as;
+} QuireEvent;
+
+#endif
