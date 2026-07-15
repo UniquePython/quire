f6c1dfce80a74284e4ad3d371a1114a1c3d1dc34
Ananyo Bhattacharya <169988112+UniquePython@users.noreply.github.com>
2026-07-15 13:17:34 +0530

    Fix coupled isatty bug and only flush for errors

--- src/quire_log.c
+++ src/quire_log.c
@@ -5,7 +5,8 @@
 #include <unistd.h>
 #include <stdbool.h>
 
-static bool gUseColor = false;
+static bool gUseColorStdout = false;
+static bool gUseColorStderr = false;
 
 #define QUIRE_COLOR_RESET "\033[0m"
 #define QUIRE_COLOR_BOLD "\033[1m"
@@ -16,31 +17,36 @@ static bool gUseColor = false;
 
 void QuireLogInit(void)
 {
-    gUseColor = isatty(fileno(stdout)) && isatty(fileno(stderr));
+    gUseColorStdout = isatty(fileno(stdout));
+    gUseColorStderr = isatty(fileno(stderr));
 }
 
 static void QuireLogWrite(
     FILE *restrict stream,
+    bool useColor,
+    bool flush,
     const char *restrict level,
     const char *restrict color,
     const char *restrict fmt,
     va_list args)
 {
-    if (gUseColor)
+    if (useColor)
         fprintf(stream, "%s%s[%s]%s ", QUIRE_COLOR_BOLD, color, level, QUIRE_COLOR_RESET);
     else
         fprintf(stream, "[%s] ", level);
 
     vfprintf(stream, fmt, args);
     fprintf(stream, "\n");
-    fflush(stream);
+
+    if (flush)
+        fflush(stream);
 }
 
 void QuireLogError(const char *fmt, ...)
 {
     va_list args;
     va_start(args, fmt);
-    QuireLogWrite(stderr, "ERROR", QUIRE_COLOR_RED, fmt, args);
+    QuireLogWrite(stderr, gUseColorStderr, true, "ERROR", QUIRE_COLOR_RED, fmt, args);
     va_end(args);
 }
 
@@ -48,7 +54,7 @@ void QuireLogInfo(const char *fmt, ...)
 {
     va_list args;
     va_start(args, fmt);
-    QuireLogWrite(stdout, "INFO", QUIRE_COLOR_YELLOW, fmt, args);
+    QuireLogWrite(stdout, gUseColorStdout, false, "INFO", QUIRE_COLOR_YELLOW, fmt, args);
     va_end(args);
 }
 
@@ -56,6 +62,6 @@ void QuireLogDebug(const char *fmt, ...)
 {
     va_list args;
     va_start(args, fmt);
-    QuireLogWrite(stdout, "DEBUG", QUIRE_COLOR_CYAN, fmt, args);
+    QuireLogWrite(stdout, gUseColorStdout, false, "DEBUG", QUIRE_COLOR_CYAN, fmt, args);
     va_end(args);
 }
