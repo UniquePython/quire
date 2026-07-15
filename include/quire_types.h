5b7a4eb4b53a2239c35d79c619cbef6241e3f222
Ananyo Bhattacharya <169988112+UniquePython@users.noreply.github.com>
2026-07-15 14:14:36 +0530

    Add typedef'd types

---
+++ include/quire_types.h
@@ -0,0 +1,23 @@
+#ifndef QUIRE_TYPES_H_
+#define QUIRE_TYPES_H_
+
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef float f32;
+typedef double f64;
+
+typedef size_t usize;
+
+#endif
